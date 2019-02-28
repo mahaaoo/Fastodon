fa84cf8a2e411ea6ab665ec02211ba481d6015b9
mah93 <303527871@qq.com>
2019-02-28 19:52:23 +0800

    添加首页时间线

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fastodon/public.dart';
 
 class Home extends StatefulWidget {
@@ -7,6 +8,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin {
+  List _homeTimeLineList = [];
+  
   @override
   bool get wantKeepAlive => true;
 
@@ -18,7 +21,7 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin {
     });
   }
 
-  Future<void> _homeTimeLine(String hostUrl) async {
+  Future<void> _homeTimeLine() async {
     Request.get();
   }
 
@@ -27,6 +30,33 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin {
     eventBus.off(EventBusKey.StorageSuccess);
     super.dispose();
   }
+// https://cmx.im/system/accounts/avatars/000/000/001/original/582dc1b4215085e0.png?1506860187
+  Widget _rowBuild(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            ClipRRect(
+              child: new CachedNetworkImage(
+                  imageUrl: "https://cmx.im/system/accounts/avatars/000/000/001/original/582dc1b4215085e0.png?1506860187",
+                  width: 50,
+                  height: 50,
+                  fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            Column(
+              children: <Widget>[
+                Text('海嘟嘟'),
+                Text('1分钟前')
+              ],
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +64,21 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin {
       appBar: AppBar(
         title: Text('首页'),
       ),
-      body: ,
+      body: RefreshIndicator(
+        onRefresh: () => _homeTimeLine(),
+        child: ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            return _rowBuild(context);
+          },
+          itemCount: 2,
+          separatorBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+              child: Divider(height: 1.0, color: MyColor.backgroundColor),
+            );
+          },
+        )
+      ),
     );
   }
 }
