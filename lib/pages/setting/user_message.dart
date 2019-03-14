06405ffcc1743b08ae6942ebf76f4fbd986c3c4e
mah93 <303527871@qq.com>
2019-03-14 19:00:18 +0800

    个人中心的头部信息

--- lib/pages/setting/user_message.dart
+++ lib/pages/setting/user_message.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 import 'package:fastodon/public.dart';
 import 'model/owner_account.dart';
 
 import 'package:fastodon/widget/avatar.dart';
 
-class UserMessage extends StatelessWidget {
+class UserMessage extends StatefulWidget {
   UserMessage({Key key, @required this.account}) : super(key: key);
 
   final OwnerAccount account;
 
+  @override
+  _UserMessageState createState() => _UserMessageState();
+
+}
+class _UserMessageState extends State<UserMessage> {
+  int _currentWidget = 0;
+
+  List<Widget> contentList = [
+    Center(
+      child: Text('11111'),
+    ),
+    Center(
+      child: Text('22222'),
+    ),
+    Center(
+      child: Text('33'),
+    ),
+  ];
+
   Widget header(BuildContext context) {
-    if (account == null) {
+    if (widget.account == null) {
       return Container(
         height: 200,
         color: MyColor.mainColor,
@@ -23,7 +43,7 @@ class UserMessage extends StatelessWidget {
       child: CachedNetworkImage(
         height: 200,
         width: Screen.width(context),
-        imageUrl: account.header,
+        imageUrl: widget.account.header,
         fit: BoxFit.cover,
       ),
     );
@@ -36,9 +56,24 @@ class UserMessage extends StatelessWidget {
           children: <Widget>[
             header(context),
             Positioned(
+              top: 50,
+              width: Screen.width(context),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Avatar(url: account.avatar)
+                  Avatar(url: widget.account.avatar),
+                  SizedBox(height: 5),
+                  Text(
+                    widget.account.displayName, 
+                    style: TextStyle(color: MyColor.widgetDefaultColor, fontSize: 15),
+                  ),
+                  Container(
+                    width: Screen.width(context) - 60,
+                    child: Html(
+                      data: widget.account.note,
+                      defaultTextStyle: TextStyle(color: MyColor.widgetDefaultColor, fontSize: 15)
+                    ),
+                  )
                 ],
               ),
             ),
@@ -48,13 +83,41 @@ class UserMessage extends StatelessWidget {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: MyColor.widgetDefaultColor,
+  Widget headerSection(int number, String title) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
       child: Column(
         children: <Widget>[
-          userHeader(context)
+          Text('$number', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black)),
+          Text(title, style: TextStyle(fontSize: 13, color: MyColor.greyText))
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          userHeader(context),
+          Card(),
+          DefaultTabController(
+            length: 3,
+            child: TabBar(
+              tabs: [
+                headerSection(widget.account.statusesCount,'嘟文'),
+                headerSection(widget.account.followingCount, '关注'),
+                headerSection(widget.account.followersCount, '粉丝'),
+              ],
+              onTap: (index) {
+                setState(() {
+                  _currentWidget = index;
+                });
+              },
+            ),
+          ),
+          contentList[_currentWidget]
         ],
       ),
     );
