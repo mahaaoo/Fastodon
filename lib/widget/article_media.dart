db86c5b4685609858d715ef0d221bbe132c010f1
mah93 <303527871@qq.com>
2019-05-08 15:23:17 +0800

    添加多图片的九宫格布局

--- lib/widget/article_media.dart
+++ lib/widget/article_media.dart
@@ -12,21 +12,21 @@ class ArticleMedia extends StatelessWidget {
 
   final List itemList;
 
-  List<Widget> picAndVideo () {
+  List<Widget> picAndVideo (BuildContext context) {
     List<Widget> picList = [];
     for (var data in itemList) {
       MediaAttachment media = MediaAttachment.fromJson(data);
       if(media.type == 'image') {
         picList.add(GestureDetector(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(1.0),
-              child: CachedNetworkImage(
-                imageUrl: media.previewUrl,
-              ),
+          child: Container(
+            width: (Screen.width(context) - 50) / 3,
+            height: (Screen.width(context) - 50) / 3,
+            child: CachedNetworkImage(
+              imageUrl: media.previewUrl,
             ),
           ),
           onTap: () {
+            print('object');
           },
         ));
       } else {
@@ -39,10 +39,15 @@ class ArticleMedia extends StatelessWidget {
   @override
   Widget build(BuildContext context) { 
     return Container(
+      width: Screen.width(context),
       padding: EdgeInsets.all(15),
-      child: Column(
-        children: picAndVideo(),
-      ),
+      child: Wrap(
+        alignment: WrapAlignment.start,
+        spacing: 10,
+        runSpacing: 10,
+        crossAxisAlignment: WrapCrossAlignment.end, 
+        children: picAndVideo(context), //要显示的子控件集合
+      )
     );
   }
 }
